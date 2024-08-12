3c38c7221c867c4ce99f0a756f3c563922c3f037
Ben Guthrie <guthrieb@hotmail.co.uk>
2024-08-11 21:46:52 -0600

    Added analytics

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,25 @@ hljs.registerLanguage('python', python)
 
 import App from './App.vue'
 import router from './router'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCq8bgL519FV6s5QiNoc_zWU6axNFfMhU0',
+  authDomain: 'benguthriesite.firebaseapp.com',
+  projectId: 'benguthriesite',
+  storageBucket: 'benguthriesite.appspot.com',
+  messagingSenderId: '355003784830',
+  appId: '1:355003784830:web:69b6373fdd5511192c022f',
+  measurementId: 'G-VB4DRWFDWP'
+}
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig)
+const analytics = getAnalytics(firebaseApp)
 
 const app = createApp(App)
 
